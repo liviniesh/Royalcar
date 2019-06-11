84a16f4ef2f5c1efc2e888787d5868beb2e335b4
liviniesh <liviniesh@gmail.com>
2019-06-11 13:40:19 +0800

    remove folder

--- royalcar/php/verify_email.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "slumberjer@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: noreply@uumresearch.com" . "\r\n" .
-"CC: ahmadhanis@uum.edu.my";
-
-mail($to,$subject,$txt,$headers);
-?>
